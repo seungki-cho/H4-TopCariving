2775f39199efd5c6db2e04eebbeec365af8c6a42
mingu <96288558+devMingu@users.noreply.github.com>
2023-08-01 16:54:19 +0900

    init: typo 세팅

---
+++ front/src/styles/typo.tsx
@@ -0,0 +1,120 @@
+import { css } from '@emotion/react';
+
+const hyundaiSans = css`
+  font-family: url('../assets/font/HyundaiSansHead-Regular.otf');
+`;
+
+export const typo = {
+  Heading1_Bold: css`
+    ${hyundaiSans}
+    font-weight: 700;
+    font-size: 26px;
+    line-height: 36px;
+  `,
+  Heading2_Bold: css`
+    ${hyundaiSans}
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 32px;
+  `,
+  Heading3_Bold: css`
+    ${hyundaiSans}
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 28px;
+  `,
+  Heading4_Bold: css`
+    ${hyundaiSans}
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 24px;
+  `,
+
+  Heading1_Medium: css`
+    ${hyundaiSans}
+    font-weight: 500;
+    font-size: 26px;
+    line-height: 36px;
+  `,
+  Heading2_Medium: css`
+    ${hyundaiSans}
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 32px;
+  `,
+  Heading3_Medium: css`
+    ${hyundaiSans}
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 28px;
+  `,
+  Heading4_Medium: css`
+    ${hyundaiSans}
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 24px;
+  `,
+
+  Body1_Regular: css`
+    ${hyundaiSans}
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 24px;
+  `,
+  Body2_Regular: css`
+    ${hyundaiSans}
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 24px;
+  `,
+  Body3_Regular: css`
+    ${hyundaiSans}
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 22px;
+  `,
+  Body4_Regular: css`
+    ${hyundaiSans}
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 20px;
+  `,
+
+  Body1_Medium: css`
+    ${hyundaiSans}
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 24px;
+  `,
+  Body2_Medium: css`
+    ${hyundaiSans}
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+  `,
+  Body3_Medium: css`
+    ${hyundaiSans}
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 22px;
+  `,
+  Body4_Medium: css`
+    ${hyundaiSans}
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 20px;
+  `,
+
+  Caption_Regular: css`
+    ${hyundaiSans}
+    font-weight: 400;
+    font-size: 10px;
+    line-height: 20px;
+  `,
+  Caption_Medium: css`
+    ${hyundaiSans}
+    font-weight: 500;
+    font-size: 10px;
+    line-height: 20px;
+  `,
+};
