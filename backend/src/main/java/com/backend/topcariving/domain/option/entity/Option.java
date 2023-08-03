b83cc494e537caae91979422db9bdf2bcc5ac3d1
kimahhh <kimahyunn132@gmail.com>
2023-08-03 11:53:40 +0900

    style: @Column 어노테이션 삭제
    Co-authored-by: kimahhh <kimahyunn132@gmail.com>
    Co-authored-by: kwYoohae <gocksdl7979@gmail.com>

--- backend/src/main/java/com/backend/topcariving/domain/option/entity/Option.java
+++ backend/src/main/java/com/backend/topcariving/domain/option/entity/Option.java
@@ -1,7 +1,6 @@
 package com.backend.topcariving.domain.option.entity;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import lombok.AccessLevel;
@@ -14,25 +13,19 @@ import lombok.NoArgsConstructor;
 public class Option {
 
 	@Id
-	@Column("option_id")
 	private Long optionId;
 
 	private String category;
 
-	@Column("category_detail")
 	private String categoryDetail;
 
-	@Column("option_name")
 	private String optionName;
 
-	@Column("option_detail")
 	private String optionDetail;
 
 	private int price;
 
-	@Column("photo_url")
 	private String photoUrl;
 
-	@Column("parent_option_id")
 	private Long parentOptionId;
 }
