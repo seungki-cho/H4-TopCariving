3fa80223ec2334b952b1a26857142375cadb5def
seungki-cho <beenznio@kookmin.ac.kr>
2023-08-06 00:31:45 +0900

    feat: tapGesture에 따라 fold, unfold

--- iOS/TopCariving/TopCariving/View/ContainerStackView.swift
+++ iOS/TopCariving/TopCariving/View/ContainerStackView.swift
@@ -12,6 +12,9 @@ class FoldableStackView: UIStackView {
     // MARK: - UI properties
     
     // MARK: - Properties
+    var bag = Set<AnyCancellable>()
+    var tapSubject = PassthroughSubject<Int, Never>()
+
     // MARK: - Lifecycles
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -34,5 +37,32 @@ class FoldableStackView: UIStackView {
         alignment = .fill
         distribution = .equalSpacing
         spacing = 8
+    }
+    
+    override func addArrangedSubview(_ view: UIView) {
+        guard let view = view as? FoldableView else { return }
+        super.addArrangedSubview(view)
+        tapped(at: 0)
+        
+        view.tapSubject
+            .compactMap { [weak self] _ in
+                self?.arrangedSubviews.firstIndex(of: view)
+            }
+            .sink(receiveValue: { [weak self] index in
+                guard let self else { return }
+                tapped(at: index)
+            })
+            .store(in: &bag)
+    }
+    
+    private func tapped(at index: Int) {
+        guard (0..<arrangedSubviews.count) ~= index else { return }
+        
+        arrangedSubviews
+            .compactMap { $0 as? Foldable }
+            .forEach { $0.fold() }
+        (arrangedSubviews[index] as? Foldable)?.unfold()
+        
+        tapSubject.send(index)
     }
 }
