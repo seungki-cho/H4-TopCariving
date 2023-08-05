cff4593d2e7027a6d0ac89f3b3797c1b66316e41
seungki-cho <beenznio@kookmin.ac.kr>
2023-08-06 00:31:22 +0900

    feat: UIStackView 상속받는 ContainerStackView

--- iOS/TopCariving/TopCariving/View/ContainerStackView.swift
+++ iOS/TopCariving/TopCariving/View/ContainerStackView.swift
@@ -7,3 +7,32 @@
 
 import UIKit
 import Combine
+
+class FoldableStackView: UIStackView {
+    // MARK: - UI properties
+    
+    // MARK: - Properties
+    // MARK: - Lifecycles
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setUI()
+    }
+    
+    convenience init(arrangedSubviews views: [FoldableView]) {
+        self.init(frame: .zero)
+        views.forEach { addArrangedSubview($0) }
+    }
+    
+    // MARK: - Helpers
+    func setUI() {
+        axis = .vertical
+        alignment = .fill
+        distribution = .equalSpacing
+        spacing = 8
+    }
+}
