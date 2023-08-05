683777bba3d182e57db0944b46d1936e73853dff
seungki-cho <beenznio@kookmin.ac.kr>
2023-08-06 00:31:54 +0900

    feat: Foldable 프로토콜

--- iOS/TopCariving/TopCariving/View/FoldableView.swift
+++ iOS/TopCariving/TopCariving/View/FoldableView.swift
@@ -4,3 +4,14 @@
 //
 //  Created by cho seungki on 2023/08/05.
 //
+
+import UIKit
+import Combine
+
+protocol Foldable {
+    var heightConstant: NSLayoutConstraint? { get set }
+    
+    func fold()
+    func unfold()
+}
+
