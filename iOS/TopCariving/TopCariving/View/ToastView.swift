48989b89db4f5018dcb4fefea3d3ac5c86a6ebbd
seungki-cho <beenznio@kookmin.ac.kr>
2023-08-06 20:37:42 +0900

    test: 옮기기위한 커밋

--- iOS/TopCariving/TopCariving/View/ToastView.swift
+++ iOS/TopCariving/TopCariving/View/ToastView.swift
@@ -5,8 +5,116 @@
 //  Created by 조승기 on 2023/08/04.
 //
 
-import Foundation
+import UIKit
 
-//class ToastView: UIView {
-//
-//}
+class ToastView: TappableView {
+    // MARK: - UI Property
+    private let contentView = {
+        let view = UIView()
+        view.backgroundColor = .hyundaiBlackGray.withAlphaComponent(0.5)
+        view.layer.cornerRadius = 4
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let closeButton = {
+        let button = UIButton(type: .close)
+        button.setTitleColor(UIColor.hyundaiLightSand, for: .normal)
+        return button
+    }()
+    
+    private let messageLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .hyundaiLightSand
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .designSystem(.init(name: .regular, size: ._12))
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+        paragraphStyle.lineHeightMultiple = 1.48
+        label.attributedText = NSMutableAttributedString(
+            string: "옵션 선택이 고민되신다면, 아카이\n빙에서 다른 차량을 구경해보세요!",
+            attributes: [NSAttributedString.Key.kern: -0.28, NSAttributedString.Key.paragraphStyle: paragraphStyle]
+        )
+        return label
+    }()
+    
+    // MARK: - Property
+    private let padding = 10.0
+    var sourceRect: CGRect?
+    private var triangleStartX: Double {
+        guard let sourceRect else { return frame.width / 2.0 }
+        if frame.maxX - 5 < sourceRect.midX { return frame.maxX - 10.0 }
+        if frame.minX + 5 > sourceRect.midX { return frame.minX }
+        return abs(frame.minX - sourceRect.midX + 5)
+    }
+    
+    // MARK: - LifeCycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUI()
+        setLayout()
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        drawTriangle()
+    }
+    
+    // MARK: - Helper
+    private func drawTriangle() {
+        let path = UIBezierPath()
+        print(triangleStartX, frame)
+        let startPoint = CGPoint(x: triangleStartX, y: padding)
+        path.move(to: startPoint)
+        
+        let leftTopPoint = startPoint.applying(.init(translationX: 4, y: -7))
+        path.addLine(to: leftTopPoint)
+        
+        let rightTopPoint = leftTopPoint.applying(.init(translationX: 2, y: 0))
+        let controlPoint = leftTopPoint.applying(.init(translationX: 1, y: -2))
+        path.addQuadCurve(to: rightTopPoint, controlPoint: controlPoint)
+
+        let endPoint = CGPoint(x: startPoint.x + 10, y: padding)
+        path.addLine(to: endPoint)
+        
+        path.close()
+        UIColor.hyundaiBlackGray.withAlphaComponent(0.5).set()
+        path.fill()
+    }
+    
+    func setUI() {
+        backgroundColor = .clear
+        addSubview(contentView)
+        [closeButton, messageLabel].forEach {
+            contentView.addSubview($0)
+        }
+    }
+    
+    func setLayout() {
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: topAnchor, constant: padding),
+            contentView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -padding),
+            contentView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            contentView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+            
+            closeButton.topAnchor.constraint(equalTo: contentView.topAnchor),
+            closeButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            closeButton.widthAnchor.constraint(equalToConstant: 20),
+            closeButton.heightAnchor.constraint(equalToConstant: 20),
+            
+            messageLabel.topAnchor.constraint(equalTo: contentView.topAnchor),
+            messageLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            messageLabel.trailingAnchor.constraint(equalTo: closeButton.leadingAnchor, constant: 7.5),
+            messageLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor)
+        ])
+    }
+}
--- iOS/TopCariving/TopCariving/ViewController/ViewController.swift
+++ iOS/TopCariving/TopCariving/ViewController/ViewController.swift
@@ -6,13 +6,92 @@
 //
 
 import UIKit
+import Combine
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    // MARK: - UI properties
+    private let rotaitionCarImageView = UIImageView()
+    private let scrollView: UIScrollView = UIScrollView()
+    private let containerStackView = FoldableStackView()
+    
+    private let testView = {
+        let view = UIButton()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .blue
+        view.addTarget(self, action: #selector(tap(sender:)), for: .touchUpInside)
+        return view
+    }()
+    
+    // MARK: - Properties
+    private var bag = Set<AnyCancellable>()
+    
+    // MARK: - Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        setUI()
+        setLayout()
+        bind()
     }
 
-
+    // MARK: - Helpers
+    private func bind() {
+        
+        let testIcons = [(URL(string: "https://topcariving.s3.ap-northeast-2.amazonaws.com/png/leBlanc1.png")! , "20인치\n알로이 휠"),
+                       (URL(string: "https://topcariving.s3.ap-northeast-2.amazonaws.com/png/leBlanc2.png")!, "서라운드 뷰\n모니터"),
+                       (URL(string: "https://topcariving.s3.ap-northeast-2.amazonaws.com/png/leBlanc3.png")!, "클러스터\n(12.3인 컬러 LCD)")]
+        (0..<4).forEach { _ in
+            let view = CarSummaryContainer()
+            view.setInfo(to: "1. Le Blanc", price: "47,720,000", icons: testIcons)
+            containerStackView.addArrangedSubview(view)
+        }
+        
+    }
+    
+    private func setUI() {
+        [scrollView, testView].forEach {
+            view.addSubview($0)
+        }
+        [containerStackView].forEach {
+            scrollView.addSubview($0)
+        }
+        navigationItem.rightBarButtonItem = .init(barButtonSystemItem: .camera, target: nil, action: nil)
+    }
+    
+    private func setLayout() {
+        containerStackView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            
+            containerStackView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 300),
+            containerStackView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            containerStackView.widthAnchor.constraint(equalToConstant: view.frame.width - 32),
+            containerStackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            
+            testView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15),
+            testView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -150 ),
+            testView.widthAnchor.constraint(equalToConstant: 50),
+            testView.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    @objc private func tap(sender: UIButton) {
+        let vcc = ToastView(frame: .init(origin: .init(x: sender.frame.maxX - 200, y: sender.frame.maxY + 5),
+                                         size: .init(width: 220, height: 90)))
+        vcc.sourceRect = sender.frame
+        view.addSubview(vcc)
+        vcc
+            .tapSubject
+            .sink(receiveValue: { view in
+                guard let view = view as? TappableView else { return }
+                view.removeFromSuperview()
+                print("hi")
+            })
+            .store(in: &bag)
+    }
 }
-
