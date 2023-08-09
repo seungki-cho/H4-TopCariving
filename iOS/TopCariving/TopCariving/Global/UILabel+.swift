b20d051ba7c151cdc1789fdbddb18480c3b3559a
조승기 <beenzino@kookmin.ac.kr>
2023-08-09 20:29:17 +0900

    feat: 폰트와 AttirbutedString 동시 적용 메소드 작성

--- iOS/TopCariving/TopCariving/Global/UILabel+.swift
+++ iOS/TopCariving/TopCariving/Global/UILabel+.swift
@@ -4,3 +4,17 @@
 //
 //  Created by 조승기 on 2023/08/08.
 //
+
+import UIKit
+
+extension UILabel {
+    func setFont(to font: HyundaiFont.Custom) {
+        self.font = .designSystem(font)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+        paragraphStyle.lineHeightMultiple = font.size.lineHeight / font.size.rawValue
+        attributedText = NSAttributedString(string: text ?? "",
+                                            attributes: [NSAttributedString.Key.kern: 0,
+                                                         NSAttributedString.Key.paragraphStyle: paragraphStyle])
+    }
+}
