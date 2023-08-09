9310d42a4108fa07b75d8766bb557a975b091848
조승기 <beenzino@kookmin.ac.kr>
2023-08-09 18:10:14 +0900

    feat: 마이카 베이스 네비게이션 바 설정

--- iOS/TopCariving/TopCariving/ViewController/BaseMyCarViewController.swift
+++ iOS/TopCariving/TopCariving/ViewController/BaseMyCarViewController.swift
@@ -4,3 +4,33 @@
 //
 //  Created by 조승기 on 2023/08/09.
 //
+
+import UIKit
+
+class BaseMyCarViewController: UIViewController {
+    // MARK: - UI properties
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setNavigationBar()
+    }
+    
+    // MARK: - Helpers
+    private func setNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = .hyundaiSand
+        
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        navigationController?.navigationBar.topItem?.backButtonTitle = ""
+        navigationController?.navigationBar.tintColor = .hyundaiBlackGray
+        let topCarivingImage = UIImage(named: "TopArchivingButton")?.resized(to: 40)
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(image: topCarivingImage,
+                                                                                   style: .done,
+                                                                                   target: nil,
+                                                                                   action: nil)
+    }
+}
