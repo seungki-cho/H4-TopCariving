e2f7b0ba0d809cb97ad7e8ff33df036b71cbc3f5
조승기 <beenzino@kookmin.ac.kr>
2023-08-12 17:22:28 +0900

    feat: OptionSelectView UI

---
+++ iOS/TopCariving/TopCariving/View/OptionSelectView.swift
@@ -0,0 +1,94 @@
+//
+//  OptionSelectView.swift
+//  TopCariving
+//
+//  Created by 조승기 on 2023/08/12.
+//
+
+import Combine
+import UIKit
+class OptionSelectView: UIView {
+    // MARK: - UI properties
+    private let optionCountLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .darkGray
+        label.setFont(to: .init(name: .medium, size: ._16))
+        label.text = "선택옵션 6개"
+        return label
+    }()
+    
+    private let includedOptionButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("기본 포함 품목", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = .designSystem(.init(name: .regular, size: ._14))
+        button.layer.cornerRadius = 4
+        button.backgroundColor = .hyundaiSand
+        return button
+    }()
+    
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: self.collectionViewLayout)
+        collectionView.isScrollEnabled = true
+        collectionView.contentInset = .zero
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.register(OptionCardCell.self, forCellWithReuseIdentifier: OptionCardCell.identifier)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+    private let collectionViewLayout = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: 160, height: 197)
+        layout.sectionInset = .init(top: 0, left: 0, bottom: 0, right: 5)
+        layout.headerReferenceSize.width = 5
+        return layout
+    }()
+    
+    // MARK: - Properties
+    // MARK: - Lifecycles
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUI()
+        setLayout()
+    }
+    
+    // MARK: - Helpers
+    private func setUI() {
+        [optionCountLabel, includedOptionButton, collectionView].forEach {
+            addSubview($0)
+        }
+        backgroundColor = .white
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+    
+    private func setLayout() {
+        NSLayoutConstraint.activate([
+            optionCountLabel.topAnchor.constraint(equalTo: topAnchor, constant: 13),
+            optionCountLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            optionCountLabel.heightAnchor.constraint(equalToConstant: 24),
+            optionCountLabel.widthAnchor.constraint(equalToConstant: 100),
+            
+            includedOptionButton.topAnchor.constraint(equalTo: topAnchor, constant: 13),
+            includedOptionButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            includedOptionButton.widthAnchor.constraint(equalToConstant: 100),
+            includedOptionButton.heightAnchor.constraint(equalToConstant: 26),
+            
+            collectionView.topAnchor.constraint(equalTo: optionCountLabel.bottomAnchor, constant: 13),
+            collectionView.heightAnchor.constraint(equalToConstant: 197),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
+    
+}
