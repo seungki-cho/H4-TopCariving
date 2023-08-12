b428b6efc0fd73b1d45e5da1c8e9bb84341365dd
조승기 <beenzino@kookmin.ac.kr>
2023-08-12 17:23:14 +0900

    feat: Colletionview delegate, 옵션 데이터 소스

--- iOS/TopCariving/TopCariving/View/OptionSelectView.swift
+++ iOS/TopCariving/TopCariving/View/OptionSelectView.swift
@@ -7,6 +7,12 @@
 
 import Combine
 import UIKit
+
+protocol OptionSelectViewDataSource: NSObject {
+    func numberOfOption() -> Int
+    func optionSelectView(willShowCellAt indexPath: IndexPath) -> OptionCardCell.OptionCardViewModel
+}
+
 class OptionSelectView: UIView {
     // MARK: - UI properties
     private let optionCountLabel: UILabel = {
@@ -49,6 +55,11 @@ class OptionSelectView: UIView {
     }()
     
     // MARK: - Properties
+    weak var datasource: OptionSelectViewDataSource?
+    private var selectedIndexBag = [IndexPath: AnyCancellable]()
+    var tapAddButtonSubject = PassthroughSubject<IndexPath, Never>()
+    var tapCellSubject = PassthroughSubject<IndexPath, Never>()
+    
     // MARK: - Lifecycles
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -91,4 +102,52 @@ class OptionSelectView: UIView {
         ])
     }
     
+    func refresh() {
+        collectionView.reloadData()
+        optionCountLabel.text = "선택옵션 \(datasource?.numberOfOption() ?? 0)개"
+    }
+}
+
+extension OptionSelectView: UICollectionViewDelegate {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        didEndDisplaying cell: UICollectionViewCell,
+        forItemAt indexPath: IndexPath
+    ) {
+        selectedIndexBag[indexPath] = nil
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        tapCellSubject.send(indexPath)
+    }
+}
+
+extension OptionSelectView: UICollectionViewDataSource {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
+        datasource?.numberOfOption() ?? 0
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: OptionCardCell.identifier,
+            for: indexPath) as? OptionCardCell,
+              let datasource = datasource else { return UICollectionViewCell() }
+        
+        cell.setup(with: datasource.optionSelectView(willShowCellAt: indexPath))
+        
+        selectedIndexBag[indexPath] = cell.tapAddButtonPublisher
+            .sink(receiveValue: { [weak self] in
+                guard let self else { return }
+                cell.isAdded.toggle()
+                self.tapAddButtonSubject.send(indexPath)
+            })
+        
+        return cell
+    }
 }
