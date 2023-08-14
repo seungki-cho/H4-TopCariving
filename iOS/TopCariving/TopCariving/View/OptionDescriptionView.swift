abdebbbb7ae41b5fffa39ff22b5b55247956ec99
조승기 <beenzino@kookmin.ac.kr>
2023-08-14 21:39:44 +0900

    feat: OptionDescriptionView UI

---
+++ iOS/TopCariving/TopCariving/View/OptionDescriptionView.swift
@@ -0,0 +1,109 @@
+//
+//  OptionDescriptionView.swift
+//  TopCariving
+//
+//  Created by 조승기 on 2023/08/14.
+//
+
+import UIKit
+
+class OptionDescriptionView: UIView, Foldable {
+    // MARK: - UI properties
+    private let titleLabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.setFont(to: .init(name: .medium, size: ._18))
+        label.textColor = .hyundaiPrimaryBlue
+        label.textAlignment = .left
+        return label
+    }()
+    
+    private let separator = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .hyundaiPrimaryBlue
+        return view
+    }()
+    
+    private let descriptionLabel = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.textColor = .hyundaiPrimaryBlue
+        textView.layer.cornerRadius = 13
+        textView.backgroundColor = .clear
+        textView.isSelectable = false
+        textView.font = .designSystem(.init(name: .regular, size: ._14))
+        textView.textContainerInset = .zero
+        return textView
+    }()
+    
+    // MARK: - Properties
+    var heightConstant: NSLayoutConstraint?
+    
+    // MARK: - Lifecycles
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUI()
+        setLayout()
+    }
+    
+    // MARK: - Helpers
+    private func setUI() {
+        translatesAutoresizingMaskIntoConstraints = false
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.hyundaiPrimaryBlue.cgColor
+        layer.cornerRadius = 8
+        backgroundColor = .hyundaiPrimaryBlue.withAlphaComponent(0.1)
+        [titleLabel, separator, descriptionLabel].forEach {
+            addSubview($0)
+        }
+    }
+    
+    private func setLayout() {
+        heightConstant = heightAnchor.constraint(greaterThanOrEqualToConstant: 70)
+        heightConstant?.isActive = true
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 24),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 32),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -32),
+            titleLabel.heightAnchor.constraint(greaterThanOrEqualToConstant: 24),
+            
+            separator.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
+            separator.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 32),
+            separator.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -32),
+            separator.heightAnchor.constraint(equalToConstant: 1),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: separator.bottomAnchor, constant: 8),
+            descriptionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 32),
+            descriptionLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -32),
+            descriptionLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10)
+        ])
+    }
+    
+    func setup(with model: OptionDescriptionViewModel) {
+        titleLabel.text = model.title
+        descriptionLabel.text = model.optionDescription
+        (model.optionDescription == "-" ? fold() : unfold())
+    }
+    
+    func fold() {
+        heightConstant?.constant = 70
+        [separator, descriptionLabel].forEach {
+            $0.isHidden = true
+        }
+    }
+    
+    func unfold() {
+        heightConstant?.constant = descriptionLabel.contentSize.height + 110
+        [separator, descriptionLabel].forEach {
+            $0.isHidden = false
+        }
+    }
+}
