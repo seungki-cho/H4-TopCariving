a314ecca6758a224c997745211607050e54d73df
seungki-cho <beenznio@kookmin.ac.kr>
2023-08-15 18:41:31 +0900

    feat: 테스트 뷰 컨트롤러 작성

--- iOS/TopCariving/TopCariving/ViewController/ViewController.swift
+++ iOS/TopCariving/TopCariving/ViewController/ViewController.swift
@@ -10,21 +10,28 @@ import UIKit
 
 class ViewController: BaseMyCarViewController {
     var bag: Set<AnyCancellable> = .init()
-    var myView: UIView = {
-        var view: UIView = UIView(frame: .init(x: 100, y: 150, width: 200, height: 200))
-        view.backgroundColor = .red
-        return view
-    }()
+    private let colorSelectionView = ColorSelectionView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(myView)
-        
-        myView.throttleTapGesturePublisher().sink { _ in
-            print("hi")
-        }.store(in: &bag)
-        
         view.backgroundColor = .white
+        view.addSubview(colorSelectionView)
         
+        NSLayoutConstraint.activate([
+            colorSelectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            colorSelectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            colorSelectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            colorSelectionView.heightAnchor.constraint(equalToConstant: 92)
+        ])
+        
+        let images = (0..<19).map { "\($0)" }
+        let names = (0..<19).map { "문라이트 블루펄\($0)" }
+        colorSelectionView.setCategoryName(to: "외장")
+        colorSelectionView.refresh(by: images)
+        colorSelectionView.setColorName(to: "문라이트 블루펄")
+        
+        colorSelectionView.tapColorSubject.sink(receiveValue: {
+            self.colorSelectionView.setColorName(to: names[$0.row])
+        }).store(in: &bag)
     }
 }
