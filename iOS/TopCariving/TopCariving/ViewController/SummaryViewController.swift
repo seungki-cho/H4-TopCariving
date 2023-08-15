2f7a4f9aaca51cfa7f7305bfc710ac2382c0cae6
seungki-cho <beenznio@kookmin.ac.kr>
2023-08-16 01:45:28 +0900

    feat: 테이블 뷰 델리게이트 포함하여 UI

--- iOS/TopCariving/TopCariving/ViewController/SummaryViewController.swift
+++ iOS/TopCariving/TopCariving/ViewController/SummaryViewController.swift
@@ -4,3 +4,75 @@
 //
 //  Created by cho seungki on 2023/08/15.
 //
+
+import UIKit
+
+class SummaryViewController: UIViewController {
+    // MARK: - UI Property
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.setFont(to: .init(name: .medium, size: ._18))
+        label.text = "견적요약보기"
+        return label
+    }()
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(SummaryCell.self, forCellReuseIdentifier: SummaryCell.identifier)
+        tableView.register(SummaryHeaderView.self, forHeaderFooterViewReuseIdentifier: SummaryHeaderView.identifier)
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+    }()
+    // MARK: - Property
+    
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        setLayout()
+    }
+    
+    // MARK: - Helper
+    private func setUI() {
+        [titleLabel, tableView].forEach {
+            view.addSubview($0)
+        }
+    }
+    private func setLayout() {
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 18),
+            titleLabel.heightAnchor.constraint(equalToConstant: 24),
+            
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
+
+extension SummaryViewController: UITableViewDelegate {
+    header
+}
+
+extension SummaryViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        tableView.dequeueReusableHeaderFooterView(withIdentifier: <#T##String#>)
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: SummaryCell.identifier,
+            for: indexPath
+        ) as? SummaryCell else { return UITableViewCell() }
+        cell.setup(with: "안녕하세요", price: "+1,234,155")
+        return cell
+    }
+}
