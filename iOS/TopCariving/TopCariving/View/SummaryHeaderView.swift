3f2093971a40a28767a819ebb56478840e742ff3
seungki-cho <beenznio@kookmin.ac.kr>
2023-08-16 01:45:01 +0900

    feat: SummaryHeaderView UI

--- iOS/TopCariving/TopCariving/View/SummaryHeaderView.swift
+++ iOS/TopCariving/TopCariving/View/SummaryHeaderView.swift
@@ -4,3 +4,67 @@
 //
 //  Created by cho seungki on 2023/08/15.
 //
+
+import UIKit
+
+class SummaryHeaderView: UITableViewHeaderFooterView {
+    // MARK: - UI Property
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.setFont(to: .init(name: .medium, size: ._14))
+        label.text = "총견적금액"
+        return label
+    }()
+    private let priceLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.setFont(to: .init(name: .bold, size: ._18))
+        label.text = "47,720,000"
+        label.isHidden = true
+        return label
+    }()
+    private let wonLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.setFont(to: .init(name: .medium, size: ._12))
+        label.text = "원"
+        label.isHidden = true
+        return label
+    }()
+    // MARK: - Property
+    static let identifier = "SummaryHeaderView"
+    
+    // MARK: - LifeCycle
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        setUI()
+        setLayout()
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUI()
+        setLayout()
+    }
+    // MARK: - Helper
+    private func setUI() {
+        [titleLabel, priceLabel, wonLabel].forEach {
+            addSubview($0)
+        }
+    }
+    private func setLayout() {
+        NSLayoutConstraint.activate([
+            titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            titleLabel.heightAnchor.constraint(equalToConstant: 20),
+            
+            priceLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            priceLabel.trailingAnchor.constraint(equalTo: wonLabel.leadingAnchor),
+            priceLabel.heightAnchor.constraint(equalToConstant: 24),
+            
+            wonLabel.centerYAnchor.constraint(equalTo: centerYAnchor, constant: 3),
+            wonLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            wonLabel.heightAnchor.constraint(equalToConstant: 12)
+        ])
+    }
+}
