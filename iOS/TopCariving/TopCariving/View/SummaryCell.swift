b0e6e47fce21e1a4c8ac851a6550e6eed157a0de
seungki-cho <beenznio@kookmin.ac.kr>
2023-08-16 01:44:53 +0900

    feat: Summary Cell UI

--- iOS/TopCariving/TopCariving/View/SummaryCell.swift
+++ iOS/TopCariving/TopCariving/View/SummaryCell.swift
@@ -5,4 +5,59 @@
 //  Created by cho seungki on 2023/08/15.
 //
 
-import Foundation
+import UIKit
+
+class SummaryCell: UITableViewCell {
+    // MARK: - UI Property
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.setFont(to: .init(name: .regular, size: ._14))
+        label.text = "펠리세이드 Le Blanc"
+        return label
+    }()
+    let priceLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.setFont(to: .init(name: .regular, size: ._14))
+        label.text = "47,3400,000 원"
+        return label
+    }()
+    
+    // MARK: - Properties
+    static let identifier = "SummaryCell"
+    
+    // MARK: - LifeCycle
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setUI()
+        setLayout()
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUI()
+        setLayout()
+    }
+    
+    // MARK: - Helper
+    private func setUI() {
+        [titleLabel, priceLabel].forEach {
+            addSubview($0)
+        }
+    }
+    private func setLayout() {
+        NSLayoutConstraint.activate([
+            titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            titleLabel.heightAnchor.constraint(equalToConstant: 22),
+            
+            priceLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            priceLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 20),
+            priceLabel.heightAnchor.constraint(equalToConstant: 22)
+        ])
+    }
+    func setup(with title: String, price: String) {
+        titleLabel.text = title
+        priceLabel.text = price
+    }
+}
