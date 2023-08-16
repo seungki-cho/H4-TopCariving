9ef0f840a85a4f4a90126a9520ebc01363f89a67
조승기 <beenzino@kookmin.ac.kr>
2023-08-16 11:33:34 +0900

    feat: SummaryVC 테스트 코드 추가

--- iOS/TopCariving/TopCariving/ViewController/ViewController.swift
+++ iOS/TopCariving/TopCariving/ViewController/ViewController.swift
@@ -16,12 +16,21 @@ class ViewController: BaseMyCarViewController {
         return view
     }()
     
+    let colorimageview = ColorImageLabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(myView)
+        view.addSubview(colorimageview)
         
-        myView.throttleTapGesturePublisher().sink { _ in
-            print("hi")
+        NSLayoutConstraint.activate([
+            colorimageview.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 48),
+            colorimageview.heightAnchor.constraint(equalToConstant: 24),
+            colorimageview.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        myView.tapPublisher().sink { _ in
+            self.colorimageview.setup(with: "외장", image: "book.closed.fill", name: "book.closed.fill")
+            self.present(SummaryViewController(), animated: true)
         }.store(in: &bag)
         
         view.backgroundColor = .white
