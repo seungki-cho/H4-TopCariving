5d40c28c37a06e989026cedfa265147528a13d66
조승기 <beenzino@kookmin.ac.kr>
2023-08-16 16:13:25 +0900

    chore: SummaryVC CollectionView로 변경

--- iOS/TopCariving/TopCariving/ViewController/SummaryViewController.swift
+++ iOS/TopCariving/TopCariving/ViewController/SummaryViewController.swift
@@ -16,16 +16,36 @@ class SummaryViewController: UIViewController {
         label.text = "견적요약보기"
         return label
     }()
-    private lazy var tableView: UITableView = {
-        let tableView = UITableView(frame: .zero, style: .grouped)
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.sectionHeaderHeight = 42
-        tableView.separatorStyle = .none
-        tableView.register(SummaryCell.self, forCellReuseIdentifier: SummaryCell.identifier)
-        tableView.register(SummaryHeaderView.self, forHeaderFooterViewReuseIdentifier: SummaryHeaderView.identifier)
-        tableView.delegate = self
-        tableView.dataSource = self
-        return tableView
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: self.collectionViewLayout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(SummaryCell.self, forCellWithReuseIdentifier: SummaryCell.identifier)
+        collectionView.register(SummaryHeaderView.self,
+                                forSupplementaryViewOfKind: "header",
+                                withReuseIdentifier: SummaryHeaderView.identifier)
+        return collectionView
+    }()
+    private let collectionViewLayout: UICollectionViewLayout = {
+        let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1),
+                                                            heightDimension: .absolute(22)))
+        let headerAnchor = NSCollectionLayoutAnchor(edges: [.top])
+        
+        let header = NSCollectionLayoutSupplementaryItem(
+            layoutSize: .init(widthDimension: .fractionalWidth(1),
+                              heightDimension: .absolute(42)),
+            elementKind: "header",
+            containerAnchor: headerAnchor
+        )
+        
+        let group = NSCollectionLayoutGroup.vertical(
+            layoutSize: .init(widthDimension: .fractionalWidth(1),
+                              heightDimension: .fractionalHeight(1)),
+            subitems: [item]
+        )
+        group.interItemSpacing = .fixed(8)
+        group.supplementaryItems = [header]
+        
+        return UICollectionViewCompositionalLayout(section: .init(group: group))
     }()
     // MARK: - Property
     
@@ -39,9 +59,11 @@ class SummaryViewController: UIViewController {
     // MARK: - Helper
     private func setUI() {
         view.backgroundColor = .blue
-        [titleLabel, tableView].forEach {
+        [titleLabel, collectionView].forEach {
             view.addSubview($0)
         }
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
     private func setLayout() {
         NSLayoutConstraint.activate([
@@ -49,39 +71,48 @@ class SummaryViewController: UIViewController {
             titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 18),
             titleLabel.heightAnchor.constraint(equalToConstant: 24),
             
-            tableView.topAnchor.constraint(equalTo: titleLabel.topAnchor, constant: 16),
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            collectionView.topAnchor.constraint(equalTo: titleLabel.topAnchor, constant: 16),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
 }
 
-extension SummaryViewController: UITableViewDelegate {
-
+extension SummaryViewController: UICollectionViewDelegate {
+    
 }
 
-extension SummaryViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        guard let header = tableView.dequeueReusableHeaderFooterView(
-            withIdentifier: SummaryHeaderView.identifier
-        ) as? SummaryHeaderView else { return nil }
+extension SummaryViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        3
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        2
+    }
+    func collectionView(
+        _ collectionView: UICollectionView,
+        viewForSupplementaryElementOfKind kind: String,
+        at indexPath: IndexPath
+    ) -> UICollectionReusableView {
+        guard let header = collectionView.dequeueReusableSupplementaryView(
+            ofKind: "header",
+            withReuseIdentifier: SummaryHeaderView.identifier,
+            for: indexPath
+        ) as? SummaryHeaderView else { return UICollectionReusableView() }
         
-        header.setup(with: "반갑습니다", price: "123,123,123")
+        header.setup(with: "총 견적 금액")
         
         return header
     }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        2
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(
-            withIdentifier: SummaryCell.identifier,
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: SummaryCell.identifier,
             for: indexPath
-        ) as? SummaryCell else { return UITableViewCell() }
-        cell.setup(with: "안녕하세요", price: "+1,234,155")
+        ) as? SummaryCell else { return UICollectionViewCell() }
         return cell
     }
 }
