cad0f34299c89914ede4a83c650049a3f8c291e9
조승기 <beenzino@kookmin.ac.kr>
2023-08-19 19:47:48 +0900

    feat: BaseMyCarVC 헤더, 푸터, 요약 구현

--- iOS/TopCariving/TopCariving/ViewController/BaseMyCarViewController.swift
+++ iOS/TopCariving/TopCariving/ViewController/BaseMyCarViewController.swift
@@ -5,17 +5,23 @@
 //  Created by 조승기 on 2023/08/09.
 //
 
+import Combine
 import UIKit
 
 class BaseMyCarViewController: UIViewController {
     // MARK: - UI properties
-    
+    let progressView = ProgressView(frame: .zero)
+    let footerView = MyCarFooterView()
     // MARK: - Properties
+    var bag = Set<AnyCancellable>()
     
     // MARK: - Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
         setNavigationBar()
+        setUI()
+        setLayout()
+        setSummary()
     }
     
     // MARK: - Helpers
@@ -33,11 +39,42 @@ class BaseMyCarViewController: UIViewController {
                                                                                    target: nil,
                                                                                    action: nil)
     }
-    
+    func setUI() {
+        view.backgroundColor = .white
+        setTitle(to: "펠리세이드")
+        [progressView, footerView].forEach {
+            view.addSubview($0)
+        }
+    }
+    func setLayout() {
+        NSLayoutConstraint.activate([
+            progressView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            progressView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            progressView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            progressView.heightAnchor.constraint(equalTo: progressView.widthAnchor, multiplier: 0.154),
+            
+            footerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            footerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            footerView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            footerView.heightAnchor.constraint(equalTo: footerView.widthAnchor, multiplier: 0.365)])
+    }
+    func setProgress(to progess: ProgressView.Progress) {
+        progressView.setImage(to: progess)
+    }
     func setTitle(to title: String) {
         let titleLabel = UILabel()
         titleLabel.setFont(to: .init(name: .medium, size: ._14))
         titleLabel.text = "펠리세이드"
         navigationItem.titleView = titleLabel
+    }
+    private func setSummary() {
+        footerView.tapSummaryButton.sink(receiveValue: { [weak self] in
+            guard let self else { return }
+            let summary = SummaryViewController()
+            summary.modalPresentationStyle = .pageSheet
+            self.present(summary, animated: true)
+//            self.navigationController?.pushViewController(SelectOptionViewController(), animated: true)
+        })
+        .store(in: &bag)
     }
 }
