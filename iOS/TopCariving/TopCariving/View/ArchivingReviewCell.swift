629ad04dc3d0eb8c0c6a2fbd118b820dc657690e
조승기 <beenzino@kookmin.ac.kr>
2023-08-22 13:08:00 +0900

    feat: ArchivingReviewCellModel 작성

--- iOS/TopCariving/TopCariving/View/ArchivingReviewCell.swift
+++ iOS/TopCariving/TopCariving/View/ArchivingReviewCell.swift
@@ -4,3 +4,13 @@
 //
 //  Created by 조승기 on 2023/08/21.
 //
+struct ArchivingReviewCellModel: Hashable {
+    let carName: String
+    let searchType: String
+    let date: Date
+    let trim: String
+    let outColorName: String
+    let inColorName: String
+    let selectOptions: [String]
+    let tags: [String]
+}
