fc9fa47b2548e14b59cb6e5d77416c222019200b
조승기 <beenzino@kookmin.ac.kr>
2023-08-23 00:08:27 +0900

    feat: ModelOptionViewModel 기반 생성

--- iOS/TopCariving/TopCariving/ViewModel/ModelOptionViewModel.swift
+++ iOS/TopCariving/TopCariving/ViewModel/ModelOptionViewModel.swift
@@ -5,4 +5,36 @@
 //  Created by 조승기 on 2023/08/22.
 //
 
+import Combine
 import Foundation
+
+class ModelOptionViewModel: ViewModelType {
+    // MARK: - Input
+    struct Input {
+        let viewDidLoadPublisher: AnyPublisher<Notification, Never>
+    }
+    // MARK: - Output
+    struct Output {
+        var model = PassthroughSubject<Model, Never>()
+    }
+    // MARK: - Dependency
+    var bag = Set<AnyCancellable>()
+    let httpClient: HTTPClientProtocol
+    
+    // MARK: - LifeCycle
+    init(httpClient: HTTPClientProtocol) {
+        self.httpClient = httpClient
+    }
+    
+    // MARK: - Helper
+    func transform(input: Input) -> Output {
+        let output = Output()
+        input.viewDidLoadPublisher.sink(receiveValue: { [weak self] _ in
+            guard let self else { return }
+            Task {
+//                httpClient.sendRequest(endPoint: <#T##EndPoint#>, responseModel: <#T##Decodable.Protocol#>)
+            }
+        }).store(in: &bag)
+        return output
+    }
+}
