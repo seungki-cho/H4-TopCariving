06bc81278d7a8f7d71bd6509957e1956b840a344
조승기 <beenzino@kookmin.ac.kr>
2023-08-22 14:24:00 +0900

    feat: ArchivingDetailViewController - 테스트 데이터

--- iOS/TopCariving/TopCariving/ViewController/ArchivingDetailViewController.swift
+++ iOS/TopCariving/TopCariving/ViewController/ArchivingDetailViewController.swift
@@ -19,6 +19,7 @@ class ArchivingDetailViewController: BaseArchivingViewController {
         setUI()
         setLayout()
         setNavigationBar()
+        test()
     }
     
     // MARK: - Helpers
@@ -46,5 +47,27 @@ class ArchivingDetailViewController: BaseArchivingViewController {
                                                                                    style: .done,
                                                                                    target: nil,
                                                                                    action: nil)
+    }
+    private func test() {
+        detailView.refresh(by: [
+            .init(image: "https://topcariving.s3.ap-northeast-2.amazonaws.com/H_Genuine_Accessories/kneewarmer.jpeg",
+                  index: 0,
+                  optionName: "적외선 무릎 워머",
+                  detailOptions: [],
+                  tags: ["어린이", "편리해요", "팬이에요"]),
+            .init(image: "https://topcariving.s3.ap-northeast-2.amazonaws.com/H_Genuine_Accessories/kneewarmer.jpeg",
+                  index: 1,
+                  optionName: "적외선 무릎 워머",
+                  detailOptions: [],
+                  tags: ["어린이", "편리해요", "팬이에요"]),
+            .init(image: "https://topcariving.s3.ap-northeast-2.amazonaws.com/H_Genuine_Accessories/kneewarmer.jpeg",
+                  index: 2,
+                  optionName: "적외선 무릎 워머",
+                  detailOptions: ["전방 충돌방지 보조", "내비게이션 기반 스마트 크루즈 컨트롤", "고속도로 주행보조 2"],
+                  tags: ["차량을 깨끗하게 유지할 수 있어요:빗자루:"])
+            
+        ],
+                           review: "승차감이 좋아요 차가 크고 운전하는 시야도 높아서 좋았어요 저는 13개월 아들이 있는데 뒤에 차시트 달아도 널널할 것 같습니다. 다른 주차 관련 옵션도 괜찮아요.")
+        
     }
 }
