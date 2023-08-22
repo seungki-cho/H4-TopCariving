20e2b8a2944526f102a0470c7fd86bbdf0e388bd
조승기 <beenzino@kookmin.ac.kr>
2023-08-22 13:07:31 +0900

    feat: ArchivingDetailCellModel 작성

--- iOS/TopCariving/TopCariving/View/ArchivingDetailCell.swift
+++ iOS/TopCariving/TopCariving/View/ArchivingDetailCell.swift
@@ -4,3 +4,10 @@
 //
 //  Created by 조승기 on 2023/08/21.
 //
+struct ArchivingDetailCellModel: Hashable {
+    let image: String
+    let index: Int
+    let optionName: String
+    let detailOptions: [String]
+    let tags: [String]
+}
