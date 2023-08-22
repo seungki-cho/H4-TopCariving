0ca4a7c19dbac0241e7068ac06277df5d0382e96
조승기 <beenzino@kookmin.ac.kr>
2023-08-23 04:28:48 +0900

    feat: SuccessResponseLong DTO

---
+++ iOS/TopCariving/TopCariving/Data/Network/DataMapping/Response/SuccessResponseLongDTO+Mapping.swift
@@ -0,0 +1,16 @@
+//
+//  SuccessResponseLongDTO+Mapping.swift
+//  TopCariving
+//
+//  Created by 조승기 on 2023/08/23.
+//
+
+import Foundation
+
+struct SuccessResponseLong: Decodable {
+    let data: Int64
+}
+
+extension SuccessResponseLong {
+    
+}
