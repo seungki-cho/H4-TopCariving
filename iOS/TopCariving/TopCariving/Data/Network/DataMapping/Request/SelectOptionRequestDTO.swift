03ca03d0749b5a7b0fa9aba857386d0d9d344a28
조승기 <beenzino@kookmin.ac.kr>
2023-08-23 10:25:59 +0900

    chore: SelectOptionRequestDTO 생성

--- iOS/TopCariving/TopCariving/Data/Network/DataMapping/Request/SelectOptionRequestDTO.swift
+++ iOS/TopCariving/TopCariving/Data/Network/DataMapping/Request/SelectOptionRequestDTO.swift
@@ -4,3 +4,11 @@
 //
 //  Created by 조승기 on 2023/08/23.
 //
+
+import Foundation
+
+struct SelectOptionRequestDTO: Encodable {
+    typealias Identifier = Int64
+    let carOptionId: Identifier
+    let archivingId: Identifier?
+}
