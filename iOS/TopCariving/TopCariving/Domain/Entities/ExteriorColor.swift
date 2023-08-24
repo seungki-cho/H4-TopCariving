4202ba7ee64c79f1937210e497606e424a50c645
조승기 <beenzino@kookmin.ac.kr>
2023-08-24 13:55:57 +0900

    feat: ExteriorColor DTO, Entity 생성

---
+++ iOS/TopCariving/TopCariving/Domain/Entities/ExteriorColor.swift
@@ -0,0 +1,16 @@
+//
+//  ExteriorColor.swift
+//  TopCariving
+//
+//  Created by 조승기 on 2023/08/24.
+//
+
+import Foundation
+
+struct ExteriorColor {
+    let carOptionId: Int64
+    let optionName: String
+    let colorUrl: String
+    let price: Int
+    let tagResponses: [Tag]
+}
