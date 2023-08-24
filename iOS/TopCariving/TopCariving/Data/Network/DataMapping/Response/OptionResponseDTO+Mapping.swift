2b48bfed3458779382a9b1ece259898707abb0ca
조승기 <beenzino@kookmin.ac.kr>
2023-08-24 13:56:18 +0900

    feat: OptionResponse DTO 구현

--- iOS/TopCariving/TopCariving/Data/Network/DataMapping/Response/OptionResponseDTO+Mapping.swift
+++ iOS/TopCariving/TopCariving/Data/Network/DataMapping/Response/OptionResponseDTO+Mapping.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct OptionResponseDTO: Decodable {
+    let carOptionId: Int64
+    let optionName: String
+    let optionDetail: String
+    let price: Int
+    let photoUrl: String
+}
+
+extension OptionResponseDTO {
+    func toDomain() -> OptionResponse {
+        .init(carOptionId: carOptionId,
+              optionName: optionName,
+              optionDetail: optionDetail,
+              price: price,
+              photoUrl: photoUrl)
+    }
+}
