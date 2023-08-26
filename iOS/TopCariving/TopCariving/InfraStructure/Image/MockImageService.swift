c2d02d9a0ce96c8645fa9878a813a89e5cb6d4a9
조승기 <beenzino@kookmin.ac.kr>
2023-08-26 23:53:31 +0900

    feat: MockImageService - 캐시 추가

--- iOS/TopCariving/TopCariving/InfraStructure/Image/MockImageService.swift
+++ iOS/TopCariving/TopCariving/InfraStructure/Image/MockImageService.swift
@@ -9,18 +9,28 @@ import UIKit
 
 class MockImageService {
     static let shared = MockImageService()
+    private let cache = ImageCacheService.shared
+    
     private init() { }
-    func loadImage(with url: String) async -> Data? {
-        guard let url = URL(string: url),
-              let response = try? await URLSession.shared.data(from: url) else { return nil}
-        return response.0
+    
+    func loadImage(with stringURL: String) async -> UIImage? {
+        if let image = cache.loadImage(key: stringURL) {
+            return image
+        }
+        
+        guard let url = URL(string: stringURL),
+              let response = try? await URLSession.shared.data(from: url),
+              let image = UIImage(data: response.0) else { return nil }
+        
+        cache.saveImage(key: stringURL, image: image)
+        
+        return image
     }
 }
 
 extension UIImage {
     static func imageService(url: String) async -> UIImage? {
-        guard let data = await MockImageService.shared.loadImage(with: url) else { return nil }
-        return UIImage(data: data)
+        await MockImageService.shared.loadImage(with: url)
     }
 }
 
@@ -28,7 +38,11 @@ extension UIImageView {
     func setAsyncImage(url: String, size: CGSize? = nil) {
         Task {
             let image = await UIImage.imageService(url: url)?.resized(to: size ?? frame.size)
-            setImage(to: image)
+            if let image = image { 
+                setImage(to: image)
+            } else {
+                setImage(to: UIImage(systemName: "questionmark.app")?.resized(to: size ?? frame.size))
+            }
         }
     }
     @MainActor private func setImage(to image: UIImage?) {
