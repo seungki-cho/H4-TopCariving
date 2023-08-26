2e35d17690aaa4d65655afc2dff86d09cb7b0f83
조승기 <beenzino@kookmin.ac.kr>
2023-08-26 23:50:56 +0900

    feat: URLSession+ async data(from:), data(for:) 추가

---
+++ iOS/TopCariving/TopCariving/Global/URLSession+.swift
@@ -0,0 +1,40 @@
+//
+//  URLSession+.swift
+//  TopCariving
+//
+//  Created by 조승기 on 2023/08/26.
+//
+
+import Foundation
+
+extension URLSession {
+    func data(from url: URL) async throws -> (Data, URLResponse) {
+        try await withCheckedThrowingContinuation { continuation in
+            let task = self.dataTask(with: url) { data, response, error in
+                guard let data = data, let response = response else {
+                    let error = error ?? URLError(.badServerResponse)
+                    return continuation.resume(throwing: error)
+                }
+                
+                continuation.resume(returning: (data, response))
+            }
+            
+            task.resume()
+        }
+    }
+    
+    func data(for request: URLRequest) async throws -> (Data, URLResponse) {
+        try await withCheckedThrowingContinuation { continuation in
+            let task = self.dataTask(with: request) { data, response, error in
+                guard let data = data, let response = response else {
+                    let error = error ?? URLError(.badServerResponse)
+                    return continuation.resume(throwing: error)
+                }
+                
+                continuation.resume(returning: (data, response))
+            }
+            
+            task.resume()
+        }
+    }
+}
