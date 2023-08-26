f3d066c31d349990220712b0af7f8f57d006b364
조승기 <beenzino@kookmin.ac.kr>
2023-08-26 23:56:14 +0900

    chore: ImageService - extension 이동

--- iOS/TopCariving/TopCariving/InfraStructure/Image/MockImageService.swift
+++ iOS/TopCariving/TopCariving/InfraStructure/Image/MockImageService.swift
@@ -27,25 +27,3 @@ class MockImageService {
         return image
     }
 }
-
-extension UIImage {
-    static func imageService(url: String) async -> UIImage? {
-        await MockImageService.shared.loadImage(with: url)
-    }
-}
-
-extension UIImageView {
-    func setAsyncImage(url: String, size: CGSize? = nil) {
-        Task {
-            let image = await UIImage.imageService(url: url)?.resized(to: size ?? frame.size)
-            if let image = image { 
-                setImage(to: image)
-            } else {
-                setImage(to: UIImage(systemName: "questionmark.app")?.resized(to: size ?? frame.size))
-            }
-        }
-    }
-    @MainActor private func setImage(to image: UIImage?) {
-        self.image = image
-    }
-}
